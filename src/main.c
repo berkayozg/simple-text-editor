b8eeb0368e6237e35b5452da81341d4dcf42090c
Berkay Özgür <berkayozg@gmail.com>
2025-07-01 18:02:51 +0200

    Initial commit with /src

---
+++ src/main.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <termios.h>
+
+struct termios orig_termios;
+
+void disable_raw_mode(void) {
+
+} 
+
+void enable_raw_mode(void) {
+    struct termios raw;
+
+    tcgetattr(STDIN_FILENO, &raw);
+
+    raw.c_lflag &= ~(ECHO);
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+int main(void) {
+    enable_raw_mode();
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    return 0;
+}
