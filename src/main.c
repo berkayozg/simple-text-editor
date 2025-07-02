52cb0c4e596ba4405e5df4f203a591702e3857e4
Berkay Özgür <berkayozg@gmail.com>
2025-07-02 17:41:45 +0200

    Create helper functions for entering and exiting raw mode

--- src/main.c
+++ src/main.c
@@ -1,26 +1,58 @@
 #include <unistd.h>
+#include <errno.h>
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 
+/*** data ***/
 struct termios orig_termios;
 
+/*** terminal ***/
+void die (const char *s) {
+    perror(s);
+    exit(1);
+}
+
 void disable_raw_mode(void) {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) {
+        die("tcsetattr");
+    }
 } 
 
 void enable_raw_mode(void) {
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) {
+        die("tcgetattr");
+    }
+    atexit(disable_raw_mode);
 
     struct termios raw = orig_termios;
+    raw.c_iflag &= ~(INPCK | ICRNL | BRKINT | ISTRIP | IXON);
+    raw.c_cflag |= (CS8);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
 
-    raw.c_lflag &= ~(ECHO);
-
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
+        die ("tcsetattr");
+    }
 }
 
+/*** init ***/
 int main(void) {
     enable_raw_mode();
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    while (1) {
+        char c;
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
+            die("read");
+        }
+        if (iscntrl(c)) {
+            printf("%d\r\n", c);
+        } else {
+            printf("%d ('%c')\r\n", c, c);
+        }
+        if (c == 'q') break;
+    }
     return 0;
 }
