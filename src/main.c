7e993203431ff95ff5c3266885910c59c6e3dbfc
Berkay Özgür <berkayozg@gmail.com>
2025-07-03 14:45:44 +0200

    Clear the terminal for the editor

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+/*** includes ***/
 #include <unistd.h>
 #include <errno.h>
 #include <ctype.h>
@@ -5,11 +6,15 @@
 #include <stdlib.h>
 #include <termios.h>
 
+/*** defines ***/
+#define CTRL_KEY(k) ((k) & 0x1f)
 /*** data ***/
 struct termios orig_termios;
 
 /*** terminal ***/
 void die (const char *s) {
+    write (STDOUT_FILENO, "\x1b[2J", 4);
+    write (STDOUT_FILENO, "\x1b[H", 3);
     perror(s);
     exit(1);
 }
@@ -20,7 +25,7 @@ void disable_raw_mode(void) {
     }
 } 
 
-void enable_raw_mode(void) {
+void enable_raw_mode(void) { // Default mode is called 'Cooked' mode.
     if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) {
         die("tcgetattr");
     }
@@ -39,20 +44,54 @@ void enable_raw_mode(void) {
     }
 }
 
+char editor_read_key (void) {
+    int nread;
+    char c;
+
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+        if (nread == -1 && errno != EAGAIN) {
+            die("editor_read_key");
+        }
+    }
+    return c;
+}
+
+
+/*** output ***/
+void editor_draw_rows (void) {
+    for (int i = 0; i < 24; i++) {
+        write (STDOUT_FILENO, "~\r\n", 3);
+    }
+}
+
+void editor_refresh_screen (void) {
+    write (STDOUT_FILENO, "\x1b[2J", 4);
+    write (STDOUT_FILENO, "\x1b[H", 3);
+    editor_draw_rows();
+     
+    write (STDOUT_FILENO, "\x1b[H", 3);
+}
+
+/*** input ***/
+void editor_process_keypress (void) {
+    char c = editor_read_key();
+
+    switch (c) {
+        case CTRL_KEY('d'):
+            write (STDOUT_FILENO, "\x1b[2J", 4);
+            write (STDOUT_FILENO, "\x1b[H", 3);
+            exit(0);
+            break;
+    }
+}
+
 /*** init ***/
 int main(void) {
     enable_raw_mode();
     while (1) {
-        char c;
-        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
-            die("read");
-        }
-        if (iscntrl(c)) {
-            printf("%d\r\n", c);
-        } else {
-            printf("%d ('%c')\r\n", c, c);
-        }
-        if (c == 'q') break;
+        editor_refresh_screen();
+        editor_process_keypress();
     }
+
     return 0;
 }
