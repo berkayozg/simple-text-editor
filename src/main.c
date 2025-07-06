537128fb3aeb04ebd56625802958dafdafe932bb
Berkay Özgür <berkayozg@gmail.com>
2025-07-06 20:38:25 +0200

    Add support for opening a text file

--- src/main.c
+++ src/main.c
@@ -5,10 +5,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
+#include <sys/types.h>
 #include <termios.h>
 #include <unistd.h>
 
 /*** defines ***/
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
 #define EDITOR_VERSION "0.0.1"
 #define CTRL_KEY(k) ((k) & 0x1f)
 #define ABUF_INIT {NULL, 0}
@@ -25,10 +30,17 @@ enum EditorKey {
     PAGE_DOWN
 };
 /*** data ***/
+typedef struct e_row {
+    int size;
+    char *chars;
+} e_row;
+
 struct editorConfig {
     int cursor_x, cursor_y;
     int screen_rows;
     int screen_cols;
+    int num_rows;
+    e_row *row;
     struct termios orig_termios;
 };
 
@@ -188,6 +200,37 @@ int get_window_size(int *rows, int *cols) {
     }
 }
 
+/*** row operations  ***/
+void editor_append_row(char *s, size_t len) {
+    E.row = realloc(E.row, sizeof(e_row) * (E.num_rows + 1));
+
+    int at = E.num_rows;
+    E.row[at].size = len;
+    E.row[at].chars = malloc(len + 1);
+    memcpy(E.row[at].chars, s, len);
+    E.row[at].chars[len] = '\0';
+    E.num_rows++;
+}
+/*** file i/o ***/
+void editor_open(char *file_name) {
+    FILE *fp = fopen(file_name, "r");
+    if (!fp) {
+        die("fopen");
+    }
+    char *line = NULL;
+    size_t line_cap = 0;
+    ssize_t line_len;
+    while ((line_len = getline(&line, &line_cap, fp)) != -1) {
+        while (line_len > 0 && (line[line_len - 1] == '\n' || line[line_len - 1] == '\r')) {
+            line_len--;
+        }
+
+        editor_append_row(line, line_len);
+    }
+    free(line);
+    fclose(fp);
+}
+
 /*** append buffer ***/
 
 void abuf_append(struct abuf *ab, const char *s, int len) {
@@ -208,24 +251,32 @@ void abuf_free(struct abuf *ab) {
 /*** output ***/
 void editor_draw_rows(struct abuf *ab) {
     for (int i = 0; i < E.screen_rows; i++) {
-        if (i == E.screen_rows / 3) {
-            char welcome[80];
-            int welcome_len = snprintf(welcome, sizeof(welcome),
-                                       "Text editor -- version %s", EDITOR_VERSION);
-            if (welcome_len > E.screen_cols) {
-                welcome_len = E.screen_cols;
-            }
-            int padding = (E.screen_cols - welcome_len) / 2;
-            if (padding) {
+        if (i >= E.num_rows) {
+            if (E.num_rows == 0 && i == E.screen_rows / 3) {
+                char welcome[80];
+                int welcome_len = snprintf(welcome, sizeof(welcome),
+                                           "Text editor -- version %s", EDITOR_VERSION);
+                if (welcome_len > E.screen_cols) {
+                    welcome_len = E.screen_cols;
+                }
+                int padding = (E.screen_cols - welcome_len) / 2;
+                if (padding) {
+                    abuf_append(ab, "~", 1);
+                    padding--;
+                }
+                while (padding--) {
+                    abuf_append(ab, " ", 1);
+                }
+                abuf_append(ab, welcome, welcome_len);
+            } else {
                 abuf_append(ab, "~", 1);
-                padding--;
             }
-            while (padding--) {
-                abuf_append(ab, " ", 1);
-            }
-            abuf_append(ab, welcome, welcome_len);
         } else {
-            abuf_append(ab, "~", 1);
+            int len = E.row[i].size;
+            if (len > E.screen_cols) {
+                len = E.screen_cols;
+            }
+            abuf_append(ab, E.row[i].chars, len);
         }
 
         abuf_append(ab, "\x1b[K", 3);
@@ -315,14 +366,18 @@ void editor_process_keypress(void) {
 void init_editor(void) {
     E.cursor_x = 0;
     E.cursor_y = 0;
+    E.num_rows = 0;
+    E.row = NULL;
     if (get_window_size(&E.screen_rows, &E.screen_cols) == -1) {
         die("init");
     }
 }
-int main(void) {
+int main(int argc, char *argv[]) {
     enable_raw_mode();
     init_editor();
-
+    if (argc >= 2) {
+        editor_open(argv[1]);
+    }
     while (1) {
         editor_refresh_screen();
         editor_process_keypress();
